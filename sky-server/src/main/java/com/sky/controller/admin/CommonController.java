7f63a7875e9ec90ccc60e7a9faec56fc0b65a722
Zmos <zhangmos1899@gmail.com>
2023-12-07 18:50:13 +0100

    new dish code

---
+++ sky-server/src/main/java/com/sky/controller/admin/CommonController.java
@@ -0,0 +1,52 @@
+package com.sky.controller.admin;
+
+
+import com.sky.constant.MessageConstant;
+import com.sky.result.Result;
+import com.sky.utils.AliOssUtil;
+import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiOperation;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.multipart.MultipartFile;
+
+import java.io.File;
+import java.io.IOException;
+import java.util.UUID;
+
+@RestController
+@RequestMapping("/admin/common")
+@Api(tags = "commonInterface")
+@Slf4j
+public class CommonController {
+
+    @Autowired
+    private AliOssUtil aliOssUtil;
+
+
+    @PostMapping("/upload")
+    @ApiOperation("file upload")
+    public Result<String> upload(MultipartFile file) {
+//        log.info()
+
+
+        try {
+            String originalFilename = file.getOriginalFilename();
+            String extension = originalFilename.substring(originalFilename.lastIndexOf("."));
+            String objectName = UUID.randomUUID().toString() + extension;
+            file.transferTo(new File("D:\\images\\" + objectName));
+
+
+//            String filePath = aliOssUtil.upload(file.getBytes(), objectName);
+
+            return Result.success();
+        } catch (IOException e) {
+            log.error("upload failed", e);
+        }
+
+        return Result.error(MessageConstant.UPLOAD_FAILED);
+    }
+}
