ad104b6a88e179a958d98f455b5ed566ed54be91
Zmos <zhangmos1899@gmail.com>
2024-01-09 01:53:21 +0100

    shoppingcart add code

---
+++ sky-server/src/main/java/com/sky/controller/user/ShoppingCartController.java
@@ -0,0 +1,35 @@
+package com.sky.controller.user;
+
+import com.sky.context.BaseContext;
+import com.sky.dto.ShoppingCartDTO;
+import com.sky.entity.ShoppingCart;
+import com.sky.result.Result;
+import com.sky.service.ShoppingCartService;
+import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiOperation;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.BeanUtils;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/user/shoppingCart")
+@Slf4j
+@Api(tags = "C end shoppingcart API")
+public class ShoppingCartController {
+
+    @Autowired
+    private ShoppingCartService shoppingCartService;
+    @PostMapping("/add")
+    @ApiOperation("add shoppingcart")
+    public Result add(@RequestBody ShoppingCartDTO shoppingCartDTO) {
+        shoppingCartService.addShoppingCart(shoppingCartDTO);
+        return Result.success();
+
+
+
+    }
+}
