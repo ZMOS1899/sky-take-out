84d0072ea2bd6e8d0d21eaa6cd51b3f35b318c61
Zmos <zhangmos1899@gmail.com>
2024-01-12 03:53:12 +0100

    user pay code

--- sky-server/src/main/java/com/sky/mapper/UserMapper.java
+++ sky-server/src/main/java/com/sky/mapper/UserMapper.java
@@ -10,4 +10,6 @@ public interface UserMapper {
     User getByOpenId(String openid);
 
     void insert(User user);
+    @Select("select * from user where id = #{userId}")
+    User getById(Long userId);
 }
